ed4c47c98a0aaa6da73276304e549fbb860d8e04
Alex Dumitrescu <ad1444@my.bristol.ac.uk>
2015-01-25 12:50:57 +0000

    android app added

---
+++ esa/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/alex/Desktop/esaapp/esa/platforms/android/AndroidManifest.xml \
---
+++ esa/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes.dex : \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/BuildConfig.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/CordovaApp.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/R$attr.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/R$drawable.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/R$string.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/R$xml.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/classes/com/app/esa/R.class \
+/Users/alex/Desktop/esaapp/esa/platforms/android/ant-build/dexedLibs/classes-58b52694eb4080bd24a6f0a9dfd3759b.jar \
